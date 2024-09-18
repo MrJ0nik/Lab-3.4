f47231b10b27018259ac026a5c449a077b645648
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-09-18 23:24:06 +0300

    first commit

---
+++ lab-3.4/lab-3.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x, y; 
+    double R1, R2; 
+
+    // Введення даних
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R1 = "; cin >> R1;
+    cout << "R2 = "; cin >> R2;
+
+    // Обчислення відстані до початку координат
+    double D = sqrt(x * x + y * y);
+
+    if ((D > R1 && D <= R2) && ((x >= 0 && y >= 0) || (x <= 0 && y <= 0))) 
+        cout << "yes" << endl;
+    
+    else 
+        cout << "no" << endl;
+    
+    cin.get();
+    return 0;
+}
